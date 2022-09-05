907357eccbe19a412ee1ca64d004c3a771c5cfaa
novanxyz <1784-novanxyz@users.noreply.source.golabs.io>
2022-09-05 13:17:08 +0000

    simplify transform file

--- configs/gopay_bpjs/rpt_cleaner.py
+++ configs/gopay_bpjs/rpt_cleaner.py
@@ -1,18 +1,26 @@
 import os
 import re
-from .finrec_etl import FinrectETL
 
 
-class RPTCleaner(FinrectETL):
-    def perform(self) -> list:
-        tmpfile,ext = os.path.splitext(self.source_file)
-        tmpfile += '.tsv'
+def rpt_cleaner(source_file) -> list:
+    tmpfile,ext = os.path.splitext(source_file)
+    tmpfile += '.tsv'
 
-        expr = re.compile(r'^\d{6}.*')
-        print(tmpfile)
-        with open(self.source_file, mode='r') as in_file, open(tmpfile, mode='w') as out_file:
-            for inline in in_file.readlines():
-                if expr.match(inline):
-                    out_file.write(inline)
-        
-        return [tmpfile]
+    expr = re.compile(r'^\d{6}.*')
+    with open(source_file, mode='r') as in_file, open(tmpfile, mode='w') as out_file:
+        for inline in in_file.readlines():
+            if expr.match(inline):
+                out_file.write(inline)
+    
+    return [tmpfile]
+
+
+
+def main():
+    fn = sys.argv[1]
+    ret = rpt_cleaner(fn)
+    print(ret)
+
+
+if __name__ == '__main__':
+    main()
